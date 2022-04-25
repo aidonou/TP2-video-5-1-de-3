662dcb28e2d57cb92b28f6be1e5bf1ed1bed554f
aidonou <101400213+aidonou@users.noreply.github.com>
2022-04-24 22:20:42 -0300

    Initial commit

---
+++ Assets/Scripts/Validar_contraseña.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Validar_contraseña : MonoBehaviour
+{
+    string contracorrecta;
+    string contraUsuario;
+    public Text ingresoUsuario;
+    public Text textoMensaje;
+    public GameObject cartelMensaje;
+
+    void Start()
+    {
+        contracorrecta = "123";
+        cartelMensaje.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContraseña()
+    {
+        contraUsuario = ingresoUsuario.text;
+        if (contraUsuario == contracorrecta)
+        {
+            cartelMensaje.SetActive(true);
+            textoMensaje.text = "bienvenido";
+        }
+        else
+        {
+            cartelMensaje.SetActive(true);
+            textoMensaje.text = "Contraseña incorrecta";
+        }
+    }
+}
